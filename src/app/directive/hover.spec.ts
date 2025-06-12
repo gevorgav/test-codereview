c3f2e65e49f2f36671ec8b6cadcde282763898ab
gevorgavetisyan <gevorg@superannotate.com>
2025-06-12 11:12:28 +0400

    remove unit test

--- src/app/directive/hover.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Hover } from './hover';
-
-describe('Hover', () => {
-  it('should create an instance', () => {
-    const directive = new Hover();
-    expect(directive).toBeTruthy();
-  });
-});
